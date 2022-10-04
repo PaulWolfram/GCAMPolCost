7ae7f911ddd3ce5951658d41f6a4afebb35650d4
Paul Wolfram <37191312+PaulWolfram@users.noreply.github.com>
2022-10-04 14:54:38 -0400

    Took care of some redundancies in the code

--- noH2_zero_limitBatt+DAC+BECCS_cost.R
+++ noH2_zero_limitBatt+DAC+BECCS_cost.R
@@ -31,17 +31,12 @@ df_all_y <- xmlToDataFrame(nodes = getNodeSet(xml_dataset, "//y")) # parse margi
 # x values are in Mt C and represent marginal abated emissions relative to a reference scenario at a given marginal carbon price 
 # y values are in 1990$/t C
 
+df_all_xy <- cbind(df_all_x, df_all_y) # combine both vectors
 
-# combine both vectors:
-df_all_xy <- cbind(df_all_x, df_all_y)  
+colnames(df_all_xy) <- c('Abat Mt C','Price 1990$/t C') # rename
 
-colnames(df_all_xy) <- c('Abat Mt C','Price 1990$/t C')
-
-
-# add Mt CO2 column:
 conv <- rep(44/12, times = nrow(df_all_xy))
-abat_MtCO2 <- as.numeric(df_all_xy$'Abat Mt C') * conv
-
+abat_MtCO2 <- as.numeric(df_all_xy$'Abat Mt C') * conv # add Mt CO2 column
 
 # add 2019$/tCO2 column:
 conv <- rep(12/44*1.81221, times = nrow(df_all_xy))
@@ -52,13 +47,10 @@ df_all_xy <- cbind(df_all_xy,abat_MtCO2,price_tCO2) %>%
   rename('Abat Mt CO2' = abat_MtCO2)
 # inflation factor taken from St. Louis Federal Reserve Bank: https://fred.stlouisfed.org/series/FPCPITOTLZGUSA
 
-# remove vectors that are not needed anymore
-rm(df_all_x, df_all_y)
-
+rm(df_all_x, df_all_y) # remove vectors that are not needed anymore
 
 # drop the last 704 rows which is a different dataset (regional cost curves by year, 22 time periods x 32 regions = 704 rows)
 df_all_xy <- head(df_all_xy, -704)
-
 
 # I am only interested in 2020-2050 costs so I am also dropping the time steps 2055-2100 (6 price data points x 32 regions x 10 time periods = 1920 rows)
 df_all_xy <- head(df_all_xy, -1920)
@@ -78,7 +70,7 @@ df_all_xy$Year[(nrow(df_all_xy)-(192*7)+1):(nrow(df_all_xy)-(192*6))] = "2020"
 df_all_xy <- subset(df_all_xy, Year != "n/a")
 
 
-# check that the first row of every year starts with a zero (1st price point is always zero)
+# manually check that the first row of every year starts with a zero (1st price point is always zero)
 head(subset(df_all_xy, df_all_xy$Year=="2020"))
 head(subset(df_all_xy, df_all_xy$Year=="2025"))
 head(subset(df_all_xy, df_all_xy$Year=="2030"))
@@ -87,9 +79,8 @@ head(subset(df_all_xy, df_all_xy$Year=="2040"))
 head(subset(df_all_xy, df_all_xy$Year=="2045"))
 head(subset(df_all_xy, df_all_xy$Year=="2050"))
 
-
 # create regions vector
-Region <- rep(c("Africa_Eastern", 
+Region_32 <- c("Africa_Eastern", 
                  "Africa_Northern", 
                  "Africa_Southern", 
                  "Africa_Western", 
@@ -120,17 +111,18 @@ Region <- rep(c("Africa_Eastern",
                  "South Korea", 
                  "Southeast Asia",
                  "Taiwan", 
-                 "USA"), each=6)
-Region <- rep(Region, times=7)
+                 "USA")
 
+Region_192 <- rep(Region_32, each=6)
+
+Region_1344 <- rep(Region_192, times=7)
 
 # create price level vector
-Price_level<-rep(c('0%', '20%', '40%', '60%', '80%', '100%'),times=32)
-Price_level<-rep((Price_level),times=7)
+Price_level <- rep(c('0%', '20%', '40%', '60%', '80%', '100%'), times=224)
 
 
 # bring all together into one dataframe
-df_all_xy <- cbind(df_all_xy,Region,Price_level) 
+df_all_xy <- cbind(df_all_xy, Region_1344, Price_level) 
 
 # change order of columns
 df_all_xy <- df_all_xy[,c(5,6,7,2,4,1,3)]
@@ -141,13 +133,11 @@ abat_MtCO2 <- df_all_xy$'Abat Mt CO2'
 dummy <- abat_MtCO2[-c(nrow(df_all_xy))] 
 dummy <- append(0,dummy)
 
-df_all_xy <- cbind(df_all_xy,dummy)
+#df_all_xy <- cbind(df_all_xy,dummy)
 
-delta <- df_all_xy$'Abat Mt CO2' - df_all_xy$dummy
+delta <- df_all_xy$'Abat Mt CO2' - dummy
 
-df_all_xy <- cbind(df_all_xy,delta) %>%
-  subset(select = -c(dummy))
-
+df_all_xy <- cbind(df_all_xy,delta) 
 df_all_xy$delta[df_all_xy$delta<0] <- 0
 
 
@@ -168,58 +158,25 @@ df_all_xy$'Price 1990$/t C' <- round(as.numeric(df_all_xy$'Price 1990$/t C'), di
 df_all_xy$'Mit cost trill 2019$' <- round(as.numeric(df_all_xy$'Mit cost trill 2019$'), digits=2)
 
 
-
 ##########################
 # National mitigation cost
 ##########################
 
 mit_cost_country <- rowsum(df_all_xy$'Mit cost trill 2019$',rep((1:(nrow(df_all_xy)/6)), each=6))
 
+Region_224 <- rep(Region_32, times=7)
 
-region <- rep(c("Africa_Eastern", 
-                "Africa_Northern", 
-                "Africa_Southern", 
-                "Africa_Western", 
-                "Argentina", 
-                "Australia_NZ", 
-                "Brazil", 
-                "Canada", 
-                "Central America and Caribbean", 
-                "Central Asia", 
-                "China", 
-                "Colombia", 
-                "EU-12", 
-                "EU-15", 
-                "Europe_Eastern", 
-                "Europe_Non_EU", 
-                "European Free Trade Association", 
-                "India", 
-                "Indonesia", 
-                "Japan", 
-                "Mexico", 
-                "Middle East", 
-                "Pakistan", 
-                "Russia", 
-                "South Africa", 
-                "South America_Northern", 
-                "South America_Southern", 
-                "South Asia", 
-                "South Korea", 
-                "Southeast Asia",
-                "Taiwan", 
-                "USA"), each=1)
-region <- rep(region, times=7)
+periods_7 <- rep(c("2020",
+                 "2025",
+                 "2030",
+                 "2035",
+                 "2040",
+                 "2045",
+                 "2050"), each=1)
 
+periods_224 <- rep(periods_7, each=32)
 
-year <- rep(c("2020",
-            "2025",
-            "2030",
-            "2035",
-            "2040",
-            "2045",
-            "2050"), each=32)
-
-mit_cost_country <- cbind(mit_cost_country,region,year)
+mit_cost_country <- cbind(mit_cost_country, Region_224, periods_224)
 mit_cost_country <- as.data.frame(mit_cost_country)
 colnames(mit_cost_country) <-  c("Mit cost trill 2019$","Region", "Time period")
 
@@ -229,7 +186,7 @@ mit_cost_country$'Mit cost trill 2019$' <- round(as.numeric(mit_cost_country$'Mi
 
 # add an NPV column at a discount factor of 5% (change discount_fact if needed)
 t <- rep(c(2020), each=224)
-discount_period <- as.numeric(year) - t
+discount_period <- as.numeric(periods_224) - t
 discount_fact <- rep(c(0.05), each=224)
 npv <- mit_cost_country$'Mit cost trill 2019$' / ((1+discount_fact)^discount_period)
 
@@ -247,29 +204,19 @@ mit_cost_country <- mit_cost_country[,c(3,4,2,1,5)]
 ########################
 # Global mitigation cost
 ########################
+# calculate global cost:
+mit_cost_global <- rowsum(mit_cost_country$'Mit cost trill 2019$',rep((1:(nrow(mit_cost_country)/32)), each=32))  %>%
+  as.data.frame() 
 
-mit_cost_global <- rowsum(mit_cost_country$'Mit cost trill 2019$',rep((1:(nrow(mit_cost_country)/32)), each=32)) 
-mit_cost_global <- as.data.frame(mit_cost_global)
-
-periods <- rep(c("2020",
-                 "2025",
-                 "2030",
-                 "2035",
-                 "2040",
-                 "2045",
-                 "2050"), each=1)
-
-
-mit_cost_global <- cbind(mit_cost_global,periods)
-mit_cost_global <- as.data.frame(mit_cost_global)
-colnames(mit_cost_global) <-  c("Mit cost trill 2019$", "Time period")
+# combine, rename columns, change order, and improve readability:
+mit_cost_global <- cbind(mit_cost_global, periods_7)
+colnames(mit_cost_global) <- c("Mit cost trill 2019$", "Time period")
 mit_cost_global <- mit_cost_global[,c(2,1)]
 mit_cost_global$'Mit cost trill 2019$' <- round(as.numeric(mit_cost_global$'Mit cost trill 2019$'), digits=2)
 
 # add an NPV column at a discount factor of 5% (change the discount rate if needed)
-
 t <- rep(c(2020), each=7)
-discount_period <- as.numeric(periods) - t
+discount_period <- as.numeric(periods_7) - t
 discount_fact <- rep(c(0.05), each=7)
 npv_glob <- mit_cost_global$'Mit cost trill 2019$' / ((1+discount_fact)^discount_period)
 
@@ -300,4 +247,5 @@ mit_cost_global$'NPV trill 2019$' <- round(as.numeric(mit_cost_global$'NPV trill
 mit_cost_global$'NPV % GDP' <- round(as.numeric(mit_cost_global$'NPV % GDP'), digits=2)
 
 
-write.xlsx(mit_cost_global, file = "C:/Users/wolf184/OneDrive - PNNL/Documents/Projects/HFTO/__value_paper/outputs/pol_cost.xlsx", sheetName = "#3.5", append=TRUE, col.names = TRUE, row.names = TRUE)
+write.xlsx(mit_cost_global, file = "C:/Users/wolf184/OneDrive - PNNL/Documents/Projects/HFTO/__value_paper/outputs/pol_cost.xlsx", sheetName = "#3.5", append = TRUE, col.names = TRUE, row.names = TRUE)
+
